f17fd65828adbf67fe0ed7d4e2dcca35ec9ca7ce
shakyasimha <sushakya235@gmail.com>
2025-03-20 09:29:18 +0545

    wrote some lines to parse line and args

--- main.c
+++ main.c
@@ -5,10 +5,23 @@
  *
 */
 #include <stdio.h>
+#include <stdlib.h>
 
 // Shell loop
 void lsh_loop(void) {
-	printf("Hello World\n");
+	char **line; 
+	char **args; 
+	int status; 
+
+	do {
+		printf("> ");
+		line = lsh_read_line();
+		args = lsh_split_line(line);
+		status = lsh_execute(args);
+
+		free(line);
+		free(args);
+	} while(status);
 }
 
 // Main driver function goes here
